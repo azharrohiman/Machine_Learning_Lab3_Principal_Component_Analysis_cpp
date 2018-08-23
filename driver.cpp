a816b2e88d37c4d183f1d2d86ed560d1362d423d
Azhar Rohiman <rhmmuh005@myuct.ac.za>
2018-08-23 23:42:04 +0200

    Implemented covariance matrix calculation

--- driver.cpp
+++ driver.cpp
@@ -6,7 +6,10 @@
 #include <math.h>
 #include <array>
 #include "Eigen/Dense"
+#include "Eigen/Eigenvalues"
 
+using namespace Eigen;
+using Eigen::MatrixXd;
 using namespace std;
 
 int main(int argc, char* argv[]) {
@@ -14,7 +17,8 @@ int main(int argc, char* argv[]) {
 	vector<double> january;
 	vector<double> july;
 
-	ifstream file("input.txt");
+	//ifstream file("input.txt");
+	ifstream file("test.txt");
 	//ofstream output_file;
 	//output_file.open("output.txt");
 	double jan;
@@ -25,22 +29,47 @@ int main(int argc, char* argv[]) {
 		july.push_back(jul);
 	}
 
-	/*
-	cout << "January is: " << endl;
+	double x_total = 0.0;
+	double y_total = 0.0;
 
 	for (int i = 0; i < january.size(); i++) {
-		cout << january.at(i) << endl;
+		x_total += january.at(i);
+		y_total += july.at(i);
 	}
 
-	cout << "July is: " << endl;
+	cout << "Total for January is: " << x_total << " and total for July is: " << y_total << endl;
 
-	for (int i = 0; i < july.size(); i++) {
-		cout << july.at(i) << endl;
+	double x_mean = x_total/january.size();
+	double y_mean = y_total/july.size();
+
+	cout << "Mean for January is: " << x_mean << " and mean for July is: " << y_mean << endl;
+
+	for (int i = 0; i < january.size(); i++) {
+		january.at(i) -= x_mean;
+		july.at(i) -= y_mean;
 	}
-	*/
 
-	double x_mean = 0.0;
-	double y_mean = 0.0;
+	double january_variance = 0.0;
+	double july_variance = 0.0;
+
+	double january_july_variance = 0.0;
+
+	for (int i = 0; i < january.size(); i++) {
+		january_variance += january.at(i) * january.at(i);
+		july_variance += july.at(i) * july.at(i);
+
+		january_july_variance += january.at(i) * july.at(i);
+	}
+
+	//cout << january_variance/january.size() << endl;
+
+	MatrixXd m(2, 2);
+	m(0, 0) = january_variance/(january.size()-1);
+	m(1, 0) = january_july_variance/(january.size()-1);
+	m(0, 1) = january_july_variance/(january.size()-1);
+	m(1, 1) = july_variance/(july.size()-1);
+
+	std::cout << m << std::endl;
 
 	return 0;
 }
